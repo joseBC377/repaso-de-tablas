37da76a55ce664e26bb7c97e6bf97db3420c3296
JoseBC <jbalcazar377@gmail.com>
2025-05-16 18:28:19 -0500

    framework springboot

---
+++ src/main/java/com/example/repasotablas/RepasotablasApplication.java
@@ -0,0 +1,13 @@
+package com.example.repasotablas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepasotablasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepasotablasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/repasotablas/RepasotablasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.repasotablas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepasotablasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
